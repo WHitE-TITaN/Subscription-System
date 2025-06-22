2666d2822bb8bf0d8575e6ab65c01452feee62a1
WHitE-TITaN <dk488621@gmail.com>
2025-06-22 23:59:45 +0530

    change in ui added disply for fetched comic

--- src/index.php
+++ src/index.php
@@ -2,15 +2,22 @@
 <?php
 require_once 'functions.php';
 
-$_SESSION_start();
+session_start();
 $_SESSION['action'] = 'register';
+$comicContent = fetchAndFormatXKCDData();
+
 ?>
 
-
-<div class="container" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-    <form method="post" action="registerUser/register.php">
+<div class="header" style="text-align: center; padding: 10px; background-color: #f0f0f0;">
+    <h1>Register for XKCD Comic Updates</h1>
+    <p>Enter your email to receive updates on new XKCD comics.</p>
+    <form method="post" action="registerUser/register.php" style="border: black; border-radius: 10px; border-color: black">
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required>
         <button type="submit-email">Register</button>
     </form>
+</div>
+
+<div class="container" style="padding: 0px; display: flex; justify-content: center; align-items: center; height: 100vh;">
+    <strong><?php echo $comicContent; ?></strong>
 </div>
